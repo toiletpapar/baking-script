595a397a2cc8fb11fcfe873e92c720fe518b2585
toiletpapar <tylerbill99@hotmail.com>
2019-05-17 15:01:46 -0400

    add tree parsing

--- script.py
+++ script.py
@@ -1,1 +1,8 @@
-print("Hello World")
+import requests
+from lxml import etree
+from io import StringIO
+
+r = requests.get("https://docs.python.org/3/library/xml.etree.elementtree.html#xml.etree.ElementTree.XMLParser")
+html = etree.HTML(r.text)
+
+print(etree.tostring(html, pretty_print=True, method="html"))
