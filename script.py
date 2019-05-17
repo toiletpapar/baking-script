649bf8bf450f4b515aac2bc168ef72f2d517fc83
toiletpapar <tylerbill99@hotmail.com>
2019-05-17 16:01:09 -0400

    add an example of splitting first occurrence

--- script.py
+++ script.py
@@ -5,4 +5,15 @@ from io import StringIO
 r = requests.get("https://docs.python.org/3/library/xml.etree.elementtree.html#xml.etree.ElementTree.XMLParser")
 html = etree.HTML(r.text)
 
-print(etree.tostring(html, pretty_print=True, method="html"))
+# print(html.findall(".//h2"))
+# print(etree.tostring(html, pretty_print=True, method="html"))
+
+allElements = html.findall(".//h2")
+
+# Find all elements and split on the first occurrence of a space
+for element in allElements:
+  print(element.text.split(" ", 1))
+
+# Find all elements and show each elements attributes
+# for element in allElements:
+#   for attributes in 
